0c8538df53fd085494d121eda41b877a46a94ac8
Boris Feuze <boris.feuze1980@gmail.com>
2025-08-30 08:50:21 +0200

    add data-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layouts/Mainlayout";
-import { SignIn, SignUp, CreateEvent, EventDetails } from "./components/pages";
+import {
+  SignIn,
+  SignUp,
+  CreateEvent,
+  EventDetails,
+  Home,
+} from "./components/pages";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route index element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/events/new" element={<CreateEvent />} />
-        <Route path="/events/:id" element={<EventDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />} />
+      <Route index element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/events/new" element={<CreateEvent />} />
+      <Route path="/events/:id" element={<EventDetails />} />
+    </Routes>
   );
 };
 
