dfdabec2582abf2530580cffb83cf22cdf24d2c0
Boris Feuze <boris.feuze1980@gmail.com>
2025-09-02 09:31:17 +0200

    add button-eventdatail

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
 import { getSingleEvent } from "../data";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 const EventDetails = () => {
   const [currEvent, setCurrEvent] = useState({});
-
+  const navigate = useNavigate();
   const { id } = useParams();
 
-  console.log(id);
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
   useEffect(() => {
     const abortController = new AbortController();
     (async () => {
       try {
         const currEventData = await getSingleEvent(id, abortController);
-        console.log(currEventData);
         setCurrEvent(currEventData);
       } catch (error) {
         if (error.name === "AbortError") {
@@ -30,15 +31,20 @@ const EventDetails = () => {
     };
   }, []);
   return (
-    <div className="card card-border text-black bg-base-100 w-96">
-      <div className="card-body">
-        <p>{currEvent.date}</p>
-        <h2 className="card-title">{currEvent.title}</h2>
-        <p>{currEvent.description}</p>
-        <div className="card-actions justify-end">
-          <p>{currEvent.location}</p>
+    <div className="flex flex-row justify-center pt-[7rem]">
+      <div className="card card-border text-black bg-base-100 w-96">
+        <div className="card-body">
+          <p>{currEvent.date}</p>
+          <h2 className="card-title">{currEvent.title}</h2>
+          <p>{currEvent.description}</p>
+          <div className="card-actions justify-end">
+            <p>{currEvent.location}</p>
+          </div>
+          <p>{currEvent.updatedAt}</p>
+          <button onClick={handleGoBack} className="btn">
+            Go back
+          </button>
         </div>
-        <p>{currEvent.updatedAt}</p>
       </div>
     </div>
   );
